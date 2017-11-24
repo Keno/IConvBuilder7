c590808694d80a9e4dcafbc4be7ab5b3caba2485
Keno Fischer <keno@juliacomputing.com>
2017-11-24 15:55:37 -0500

    Add autogenerated files to build

---
+++ README.md
@@ -0,0 +1,9 @@
+# IConvBuilder7
+
+[![Build Status](https://travis-ci.org/Keno/IConvBuilder7.svg?branch=master)](https://travis-ci.org/Keno/IConvBuilder7)
+
+This repository builds binary artifacts for the  project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/Keno/IConvBuilder7/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,35 @@
+using BinaryBuilder
+
+platforms = [
+  BinaryProvider.Linux(:i686, :glibc),
+  BinaryProvider.Linux(:x86_64, :glibc),
+  BinaryProvider.Linux(:aarch64, :glibc),
+  BinaryProvider.Linux(:armv7l, :glibc),
+  BinaryProvider.Linux(:powerpc64le, :glibc),
+  BinaryProvider.MacOS(),
+  BinaryProvider.Windows(:i686),
+  BinaryProvider.Windows(:x86_64)
+]
+
+sources = [
+  "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz" =>
+"ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178",
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libiconv-1.15/
+./configure --prefix=/ --host=$target
+make -j80
+make install
+
+"""
+
+products = prefix -> [
+	LibraryProduct(prefix,"libcharset"),
+	LibraryProduct(prefix,"libiconv"),
+	ExecutableProduct(prefix,"iconv")
+]
+
+autobuild(pwd(), "", platforms, sources, script, products)
+
